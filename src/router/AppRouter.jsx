fddfeeddf7e3ce28a15377fe2e057b98784b4159
juan <judagosa8520@gmail.com>
2024-09-19 17:06:47 -0500

    feat: Add AppRouter component and update CalendarApp

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,19 @@
-import { Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth"
+import { CalendarPage } from "../calendar"
 
 export const AppRouter = () => {
+  const authStatus = 'not-authenticated'
   return (
-    <Routes>AppRouter</Routes>
+    <Routes>
+      {
+        (authStatus === 'not-authenticated')
+        ? <Route path="/auth/*" element={<LoginPage />} />
+        : <Route path="/*" element={<CalendarPage />} />
+      }
+
+      <Route path="/*" element={<Navigate to={'/auth/login'} />} />
+
+    </Routes>
   )
 }
