ce19993e3e9b7add656ee7a65244359488e43233
juan <judagosa8520@gmail.com>
2024-09-20 12:19:23 -0500

    feat: Add date-fns and react-big-calendar packages as dependencies

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,51 @@
+import { addHours } from "date-fns"
+import { Calendar } from 'react-big-calendar'
 import { Navbar } from "../"
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { localizer, getMessagesEs } from "../../helpers"
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'John'
+  }
+}]
+
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({event, start, end, isSelected});
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={ getMessagesEs() }
+        eventPropGetter={ eventStyleGetter }
+      />
     </>
   )
 }
