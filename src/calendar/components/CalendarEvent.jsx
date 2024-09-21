571f9b1ca7704525fe353ec364659d6f41c00b5e
juan <judagosa8520@gmail.com>
2024-09-20 19:09:17 -0500

    Refactor: Remove console.log statement in CalendarEvent component

--- src/calendar/components/CalendarEvent.jsx
+++ src/calendar/components/CalendarEvent.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 export const CalendarEvent = ({event}) => {
-  console.log("🚀 ~ CalendarEvent ~ event:", event)
   const { title, user } = event;
   return (
     <>
