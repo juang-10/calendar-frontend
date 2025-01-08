0f63f123f44bb5bdd69c7e86be5596ef4d300fae
juan <judagosa8520@gmail.com>
2025-01-08 15:29:32 -0500

    feat: Update CalendarModal with event creation form and improved layout

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -31,9 +31,53 @@ export const CalendarModal = () => {
       overlayClassName="modal-fondo"
       closeTimeoutMS={200}
     >
-      <h1>Hola Mundo</h1>
-      <hr />
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat excepturi debitis voluptatibus neque reiciendis vero! Omnis totam ullam placeat ad consectetur dicta ex sequi, corrupti sit deserunt sapiente velit at.</p>
+      <h1> Nuevo evento </h1>
+        <hr />
+        <form className="container">
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora inicio</label>
+                <input className="form-control" placeholder="Fecha inicio" />
+            </div>
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora fin</label>
+                <input className="form-control" placeholder="Fecha inicio" />
+            </div>
+
+            <hr />
+            <div className="form-group mb-2">
+                <label>Titulo y notas</label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Título del evento"
+                    name="title"
+                    autoComplete="off"
+                />
+                <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+            </div>
+
+            <div className="form-group mb-2">
+                <textarea 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                ></textarea>
+                <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-outline-primary btn-block"
+            >
+                <i className="far fa-save"></i>
+                <span> Guardar</span>
+            </button>
+
+        </form>
     </Modal>
   )
 }
