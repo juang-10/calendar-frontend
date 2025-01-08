a9063d5767e84213ea651bbd90bba4cd41ab48f0
juan <judagosa8520@gmail.com>
2025-01-08 16:41:14 -0500

    feat: Add Spanish locale support and time selection to DatePicker in CalendarModal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,9 +1,11 @@
 import { addHours } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
-import DatePicker from "react-datepicker";
+import DatePicker, {registerLocale} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { es } from 'date-fns/locale/es';
 
+registerLocale('es', es)
 const customStyles = {
   content: {
     top: "50%",
@@ -65,6 +67,9 @@ export const CalendarModal = () => {
             className="form-control"
             onChange={ (event) => onDateChanged(event, 'start') }
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
@@ -77,6 +82,9 @@ export const CalendarModal = () => {
             className="form-control"
             onChange={ (event) => onDateChanged(event, 'end') }
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
