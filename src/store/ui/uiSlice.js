c18be51dea933bdb1268e5392de1495da8cc1f5d
juan <judagosa8520@gmail.com>
2025-01-12 08:35:53 -0500

    feat: Integrate Redux for state management and create UI slice

---
+++ src/store/ui/uiSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const uiSlice = createSlice({
+  name: 'nameTemplate',
+  initialState: {
+    isDateModalOpen: false,
+  },
+  reducers: {
+    onOpenDateModal: (state) => {
+      state.isDateModalOpen = true;
+    },
+    onCloseDateModal: (state) => {
+      state.isDateModalOpen = false;
+    },
+  },
+});
+
+// Actions creators are generated for each case reducer function
+export const { onOpenDateModal, onCloseDateModal } = uiSlice.actions;
