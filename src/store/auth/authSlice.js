b88b3896a162712982b84f1853d53852d7fac009
juan <judagosa8520@gmail.com>
2025-02-10 17:28:20 -0500

    feat: Add authSlice for authentication state management in Redux store

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    status: 'checking', // 'checking' | 'authenticated' | 'not-authenticated'
+    user: {},
+    errorMessage: undefined
+  },
+  reducers: {
+    onChecking: ( state ) => {
+      state.status = 'checking';
+      state.user = {};
+      state.errorMessage = undefined;
+    },
+    onLogin: ( state, { payload } ) => {
+      state.status = 'authenticated';
+      state.user = payload;
+      state.errorMessage = undefined;
+    }
+  },
+});
+
+// Actions creators are generated for each case reducer function
+export const { checking } = authSlice.actions;
