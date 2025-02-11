9910a22c698e33e9f6c140c2eec63656253a069a
juan <judagosa8520@gmail.com>
2025-02-11 16:32:58 -0500

    feat: Update AppRouter to handle navigation for authenticated and non-authenticated users

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,16 +14,25 @@ export const AppRouter = () => {
   
 
   if ( status === 'checking') return <h1>Cargando...</h1>
-  
+
   return (
     <Routes>
       {
         (status === 'not-authenticated')
-        ? <Route path="/auth/*" element={<LoginPage />} />
-        : <Route path="/*" element={<CalendarPage />} />
+        ? (
+          <>
+            <Route path="/auth/*" element={<LoginPage />} />
+            <Route path="/*" element={<Navigate to={'/auth/login'} />} />
+          </>
+        )
+        : (
+          <>
+            <Route path="/" element={<CalendarPage />} />
+            <Route path="/*" element={<Navigate to={'/'} />} />
+          </>
+        )
       }
 
-      <Route path="/*" element={<Navigate to={'/auth/login'} />} />
 
     </Routes>
   )
