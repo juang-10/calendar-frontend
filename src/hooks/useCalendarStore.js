15b0ea6330b753e2017010d5ad4b9c01c63d638d
juan <judagosa8520@gmail.com>
2025-02-12 19:22:11 -0500

    feat: Implement event saving with error handling and user association in useCalendarStore

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store";
 import { calendarApi } from "../api";
 import { convertEventsToDateEvents } from "../helpers";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
   
@@ -14,13 +15,21 @@ export const useCalendarStore = () => {
   }
 
   const startSavingEvent = async (calendarEvent) => {
-    // TODO: Update the event in the backend
 
-    if(calendarEvent._id) {
-      dispatch(onUpdateEvent({...calendarEvent}))
-    } else {
+    try {
+      if(calendarEvent.id) {
+        // Actualizando
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
+        dispatch(onUpdateEvent({...calendarEvent, user}))
+        return
+      }
+  
+      // Creando
       const { data } = await calendarApi.post('/events', calendarEvent) 
       dispatch(onAddNewEvent({...calendarEvent, id: data.event.id, user}))
+      
+    } catch (error) {
+      Swal.fire('Error al guardar', error.response.data.msg, 'error')
     }
   }
 
